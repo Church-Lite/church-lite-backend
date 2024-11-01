5e0eccf7181b23b8e1ed0c597700abd63d091d4c
Geovane Araujo <geovane-araujo@outlook.com>
2024-10-31 21:59:22 -0300

    Configurações de autenticação

--- src/main/java/com/smartverse/churchlitebackend/handlers/pessoa/PessoaHandlerImpl.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/pessoa/PessoaHandlerImpl.java
@@ -1,15 +1,22 @@
 package com.smartverse.churchlitebackend.handlers.pessoa;
 
-import com.smartverse.churchlitebackend_gen.SavePerson;
-import com.smartverse.churchlitebackend_gen.SavePersonInput;
-import com.smartverse.churchlitebackend_gen.SavePersonOutput;
+import com.smartverse.churchlitebackend_gen.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PessoaHandlerImpl implements SavePerson {
+public class PessoaHandlerImpl extends PersonHandler implements SavePerson {
+
 
     @Override
     public SavePersonOutput savePerson(SavePersonInput input) {
         return null;
+    }
+
+    @Override
+    public PersonDTO save(PersonDTO obj) {
+        var entity = dtoConverter.toEntity(obj);
+        entityManager.persist(entity);
+        return dtoConverter.toDTO(entity);
     }
 }
--- src/main/resources/db/migration/V20241030181135002__Create_userTable.sql
+++ src/main/resources/db/migration/V20241030181135002__Create_userTable.sql
@@ -1,13 +1,8 @@
-DO $$
-BEGIN
-    IF current_schema = 'CHURCH_LITE_ADMIN' THEN
-            CREATE TABLE IF NOT EXISTS pessoa_usuario(
-                 id uuid,
-                 name uuid,
-                 email varchar,
-                 password varchar,
-                 tenant varchar
-            );
-    END IF;
-END $$;
 
+CREATE TABLE IF NOT EXISTS pessoa_usuario(
+     id uuid,
+     name varchar,
+     email varchar,
+     password varchar,
+     tenant varchar
+);
