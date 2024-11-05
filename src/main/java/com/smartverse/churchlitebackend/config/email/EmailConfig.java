a83fa7114f4720db67a8613b082b3f78167a5cc1
Geovane Araujo <geovane-araujo@outlook.com>
2024-11-05 06:35:17 -0300

    Configurações de autenticação

--- src/main/java/com/smartverse/churchlitebackend/config/email/EmailConfig.java
+++ src/main/java/com/smartverse/churchlitebackend/config/email/EmailConfig.java
@@ -13,17 +13,15 @@ public class EmailConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("dataon.com.br");
+        mailSender.setHost("mail.dataon.com.br");
         mailSender.setPort(587);
         mailSender.setUsername("geovane.araujo@dataon.com.br");
-        mailSender.setPassword("553322@@##");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", "*");
-        props.put("mail.smtp.ssl.checkserveridentity", "false");
+        props.put("mail.smtp.starttls.enable", "false");
 
         return mailSender;
     }
