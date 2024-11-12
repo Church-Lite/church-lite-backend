bd9e45fb3509c39ee4b9bdae79a8c0a912529c9b
Geovane Araujo <geovane-araujo@outlook.com>
2024-11-12 08:02:51 -0300

    Ajustes

--- src/main/java/com/smartverse/churchlitebackend/handlers/pessoa/PessoaHandlerImpl.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/pessoa/PessoaHandlerImpl.java
@@ -1,15 +1,8 @@
 package com.smartverse.churchlitebackend.handlers.pessoa;
 
-import com.smartverse.churchlitebackend_gen.*;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PessoaHandlerImpl implements SavePerson {
+public class PessoaHandlerImpl {
 
-
-    @Override
-    public ResponseEntity<SavePersonOutput> savePerson(SavePersonInput input) {
-        return null;
-    }
 }
