eac8da6dbc137a88b017b3edfb1b21cf295c741c
Geovane-Araujo <geovane-araujo@outlook.com>
2024-11-13 21:59:27 -0300

    concluido upload de imagem

--- src/main/java/com/smartverse/churchlitebackend/handlers/s3/S3Impl.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/s3/S3Impl.java
@@ -1,22 +1,36 @@
 package com.smartverse.churchlitebackend.handlers.s3;
 
 
+import com.smartverse.churchlitebackend.services.s3.S3Service;
 import com.smartverse.churchlitebackend_gen.RequestUpload;
 import com.smartverse.churchlitebackend_gen.RequestUploadOutput;
 import com.smartverse.churchlitebackend_gen.RequestUrl;
 import com.smartverse.churchlitebackend_gen.RequestUrlOutput;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
+@RestController
 public class S3Impl implements RequestUpload, RequestUrl {
 
+    @Autowired
+    S3Service s3Service;
 
     @Override
-    public ResponseEntity<RequestUploadOutput> RequestUpload(String fileType, String fileName) {
-        return null;
+    public ResponseEntity<RequestUploadOutput> requestUpload(Integer expired, String fileName) {
+        var url = s3Service.requestUpload(fileName, expired);
+        var output = new RequestUploadOutput();
+        output.url = url.toString();
+        return ResponseEntity.ok(output);
     }
 
     @Override
-    public ResponseEntity<RequestUrlOutput> RequestUrl(String fileName) {
-        return null;
+    public ResponseEntity<RequestUrlOutput> requestUrl(Integer expired, String fileName) {
+        var url = s3Service.requestDownload(fileName, expired);
+        var output = new RequestUrlOutput();
+        output.url = url.toString();
+        return ResponseEntity.ok(output);
     }
 }
