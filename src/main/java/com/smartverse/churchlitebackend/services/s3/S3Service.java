a0b7eb2a549d01eda536cc3f07a3561964058261
Geovane-Araujo <geovane-araujo@outlook.com>
2024-11-13 22:19:08 -0300

    concluido upload de imagem

--- src/main/java/com/smartverse/churchlitebackend/services/s3/S3Service.java
+++ src/main/java/com/smartverse/churchlitebackend/services/s3/S3Service.java
@@ -1,10 +1,18 @@
 package com.smartverse.churchlitebackend.services.s3;
 
 
+import com.potatotech.authorization.exception.ServiceException;
+import com.potatotech.authorization.tenant.TenantContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
@@ -16,8 +24,8 @@ import java.time.Duration;
 public class S3Service {
 
     private static final Region REGION = Region.US_EAST_2;
-    private static final String BUCKET_NAME = "smartchurch";
     private final S3Presigner presigner;
+    private final S3Client s3Client;
 
     public S3Service() {
 
@@ -27,12 +35,20 @@ public class S3Service {
                 .region(REGION)
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
+
+        this.s3Client = S3Client.builder()
+                .region(REGION)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
     }
 
     public URL requestUpload(String objectKey, int expirationInSeconds) {
+
+        createBucket();
+
         PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(
                 builder -> builder.signatureDuration(Duration.ofSeconds(expirationInSeconds))
-                        .putObjectRequest(por -> por.bucket(BUCKET_NAME).key(objectKey))
+                        .putObjectRequest(por -> por.bucket(TenantContext.getCurrentTenant().replace("_","-").toLowerCase()).key(objectKey))
         );
         return presignedRequest.url();
     }
@@ -40,12 +56,45 @@ public class S3Service {
     public URL requestDownload(String objectKey, int expirationInSeconds) {
         PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(
                 builder -> builder.signatureDuration(Duration.ofSeconds(expirationInSeconds))
-                        .getObjectRequest(gor -> gor.bucket(BUCKET_NAME).key(objectKey))
+                        .getObjectRequest(gor -> gor.bucket(TenantContext.getCurrentTenant().replace("_","-").toLowerCase()).key(objectKey))
         );
         return presignedRequest.url();
     }
 
+    public void createBucket() {
+
+        if(verifyExistingBucket()) return;
+
+        try {
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                    .bucket(TenantContext.getCurrentTenant().replace("_","-").toLowerCase())
+                    .build();
+
+            CreateBucketResponse response = s3Client.createBucket(createBucketRequest);
+
+        } catch (S3Exception e) {
+            throw new ServiceException(HttpStatus.BAD_REQUEST,e.getMessage());
+        }
+    }
+
+    public boolean verifyExistingBucket() {
+        try {
+            HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
+                    .bucket(TenantContext.getCurrentTenant().replace("_","-").toLowerCase())
+                    .build();
+            s3Client.headBucket(headBucketRequest);
+            return true;
+        } catch (S3Exception e) {
+            if (e.awsErrorDetails().errorCode().equals("NoSuchBucket")) {
+                return false;
+            } else {
+                throw new ServiceException(HttpStatus.BAD_REQUEST,e.getMessage());
+            }
+        }
+    }
+
     public void close() {
         presigner.close();
+        s3Client.close();
     }
 }
