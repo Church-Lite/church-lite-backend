2046f836b4052d3c7a1b5abc008a447dcc7df975
Geovane Araujo <geovane-araujo@outlook.com>
2024-11-23 06:50:10 -0300

    Lançamento de receitas e despesas

--- src/main/java/com/smartverse/churchlitebackend/handlers/financial/FinancialHandlerImpl.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/financial/FinancialHandlerImpl.java
@@ -1,8 +1,25 @@
 package com.smartverse.churchlitebackend.handlers.financial;
 
+import com.smartverse.churchlitebackend.services.transactions.TransactionsService;
+import com.smartverse.churchlitebackend_gen.FinancialDTO;
 import com.smartverse.churchlitebackend_gen.FinancialHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FinancialHandlerImpl extends FinancialHandler {
+
+
+    @Autowired
+    TransactionsService financialService;
+
+    @Override
+    public FinancialDTO save(FinancialDTO obj) {
+        var entity = dtoConverter.toEntity(obj, null);
+        entityManager.persist(entity);
+        // Inclui nas transações
+        financialService.save(entity);
+        return dtoConverter.toDTO(entity, null);
+    }
+
 }
