95a4e18bb94db247193063d4799cd0f0710e5be2
Geovane Araujo <geovane-araujo@outlook.com>
2024-11-25 07:18:27 -0300

    Lançamento de receitas e despesas

---
+++ src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsCustomHandler.java
@@ -0,0 +1,9 @@
+package com.smartverse.churchlitebackend.handlers.cashtransactions;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class CashTransactionsCustomHandler {
+}
---
+++ src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsHandlerImpl.java
@@ -0,0 +1,8 @@
+package com.smartverse.churchlitebackend.handlers.cashtransactions;
+
+import com.smartverse.churchlitebackend_gen.CashTransactionsHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CashTransactionsHandlerImpl extends CashTransactionsHandler {
+}
