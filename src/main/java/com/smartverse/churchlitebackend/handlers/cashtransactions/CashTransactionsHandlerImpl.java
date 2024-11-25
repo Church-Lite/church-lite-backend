a8e3da70cfd0a3036aebdc08b5b4f56f2a597dbf
Geovane-Araujo <geovane-araujo@outlook.com>
2024-11-25 19:14:52 -0300

    Cadastro de caixas

--- src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsHandlerImpl.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsHandlerImpl.java
@@ -1,8 +1,29 @@
 package com.smartverse.churchlitebackend.handlers.cashtransactions;
 
+import com.potatotech.authorization.exception.ServiceException;
+import com.smartverse.churchlitebackend.repository.cashtransactions.CashTransactionsCustomRepository;
+import com.smartverse.churchlitebackend_gen.CashTransactionsDTO;
 import com.smartverse.churchlitebackend_gen.CashTransactionsHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CashTransactionsHandlerImpl extends CashTransactionsHandler {
+
+
+    @Autowired
+    CashTransactionsCustomRepository cashTransactionsCustomRepository;
+
+    @Override
+    public CashTransactionsDTO save(CashTransactionsDTO obj) {
+        var entity = dtoConverter.toEntity(obj, null);
+        if(cashTransactionsCustomRepository.existsByOpening(entity.getCash().getId().toString())){
+            entityManager.persist(entity);
+            return dtoConverter.toDTO(entity, null);
+        } else {
+            throw new ServiceException(HttpStatus.BAD_REQUEST, "Já existe um caixa aberto");
+        }
+
+    }
 }
