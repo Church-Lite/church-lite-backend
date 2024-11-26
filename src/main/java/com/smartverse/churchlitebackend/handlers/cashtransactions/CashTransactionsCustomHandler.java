0254e2a67a29a29472b536615b87fa5553606530
Geovane-Araujo <geovane-araujo@outlook.com>
2024-11-25 21:50:24 -0300

    Cadastro de caixas

--- src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsCustomHandler.java
+++ src/main/java/com/smartverse/churchlitebackend/handlers/cashtransactions/CashTransactionsCustomHandler.java
@@ -1,9 +1,33 @@
 package com.smartverse.churchlitebackend.handlers.cashtransactions;
 
+import com.potatotech.authorization.exception.ServiceException;
+import com.smartverse.churchlitebackend.repository.cashtransactions.CashTransactionsCustomRepository;
+import com.smartverse.churchlitebackend_gen.CashTransactionsHandler;
+import com.smartverse.churchlitebackend_gen.GetIDCashTransaction;
+import com.smartverse.churchlitebackend_gen.GetIDCashTransactionOutput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @CrossOrigin(origins = "*")
-public class CashTransactionsCustomHandler {
+public class CashTransactionsCustomHandler implements GetIDCashTransaction {
+
+    @Autowired
+    private CashTransactionsCustomRepository cashTransactionsCustomRepository;
+
+    @Override
+    public ResponseEntity<GetIDCashTransactionOutput> getIDCashTransaction(UUID cash) {
+        var output = new GetIDCashTransactionOutput();
+        output.cashTransaction = cashTransactionsCustomRepository.getLastOpeningId(cash);
+        if(output.cashTransaction == null) {
+            throw new ServiceException(HttpStatus.NOT_FOUND,"Caixa não encontrado");
+        }
+        return ResponseEntity.ok(output);
+    }
 }
--- src/main/java/com/smartverse/churchlitebackend/repository/cashtransactions/CashTransactionsCustomRepository.java
+++ src/main/java/com/smartverse/churchlitebackend/repository/cashtransactions/CashTransactionsCustomRepository.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.UUID;
+
 @Repository
 @Primary
 public interface CashTransactionsCustomRepository extends CashTransactionsRepository {
 
     @Query(nativeQuery = true, value = " select exists (select  * from cash_transactions WHERE cash = ?1) ")
-    boolean existsByOpening(String id);
+    boolean existsByOpening(UUID id);
+
+    @Query(nativeQuery = true, value = "SELECT ID FROM cash_transactions WHERE end_date is null and cash = ?1")
+    UUID getLastOpeningId(UUID id);
 }
